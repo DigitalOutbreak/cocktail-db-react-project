0eff4af2c5c6bf430fb3460675172fa515a67060
Whoppa <joeynalvarado@gmail.com>
2023-03-12 13:03:09 -0500

    init commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur at
+        fugiat quidem nam harum minima non quisquam dolores ut quis sapiente
+        illum soluta excepturi labore maxime, blanditiis tempora corporis
+        maiores neque laboriosam sed ducimus! Minus voluptate eaque rem amet
+        quas.
+      </p>
+    </section>
+  );
+};
+
+export default About;
